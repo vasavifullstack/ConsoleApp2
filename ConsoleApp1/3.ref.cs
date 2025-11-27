2814f9b7e7327b5567f147a9926240cddfd7f86e
vasavifullstack <vasavi.fullstack@gmail.com>
2025-11-27 20:59:44 +0530

    example

---
+++ ConsoleApp1/3.ref.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 10;
+        ModifyRef(ref number);
+
+        Console.WriteLine(number);  // Output: 20
+    }
+
+    static void ModifyRef(ref int x)
+    {
+        x = x * 2; // modifies original value
+    }
+}
