561fcf713ba0c79e8fb38c3681e03d3a388696e6
vasavifullstack <vasavi.fullstack@gmail.com>
2025-11-27 21:06:31 +0530

    methods

---
+++ ConsoleApp1/4.out_method.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Programs
+{
+    static void Main()
+    {
+        int result;  // no need to initialize
+        Calculate(out result);
+
+        Console.WriteLine(result); // Output: 50
+    }
+
+    static void Calculate(out int value)
+    {
+        value = 50; // must assign before method ends
+    }
+    
+
+}
