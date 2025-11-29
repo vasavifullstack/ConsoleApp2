b524a5e22534df242a1aea921af1bb277dfc6f68
vasavifullstack <vasavi.fullstack@gmail.com>
2025-11-29 17:26:06 +0530

    Params

---
+++ ConsoleApp1/9.Async_example.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASyncOtherExmapleContainer
+{
+    class Coffeshop
+    {
+        public static async Task CallingFreindPeterAtCoffeshop()
+        {
+            Console.WriteLine("Calling Peter freind");
+            await Task.Delay(5000);
+            Console.WriteLine("Peter - Freind came");
+        }
+
+        public static async Task CallingFreindJohnAtCoffeshop()
+        {
+            Console.WriteLine("Calling John freind");
+            await Task.Delay(10000);
+
+
+            Console.WriteLine("Freind John came");
+        }
+
+        public static async Task CallingFreindRamAtCoffeshop()
+        {
+            Console.WriteLine("Calling Ram freind");
+            await Task.Delay(8000);
+            Console.WriteLine("Freind Ram came");
+        }
+
+    }
+
+    class Program
+    {
+        public static async Task Main()
+        {
+            //await Coffeshop.CallingFreindPeterAtCoffeshop();  // Correct the way you are calling ayync method thats correct.
+
+            //Coffeshop.CallingFreindJohnAtCoffeshop();
+
+            Task callingPeter = Coffeshop.CallingFreindPeterAtCoffeshop();
+            Task callingJohn = Coffeshop.CallingFreindJohnAtCoffeshop();
+            Task callingRam = Coffeshop.CallingFreindRamAtCoffeshop();
+
+            await Task.WhenAll(callingPeter, callingJohn, callingRam);
+
+        }
+    }
+
+
+
+    //Task:Cokking , Task:chatfreind , Task:lisitnmusic -- concurenlty i can do that 
+
+
+}
