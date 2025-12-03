a7b4951a0adbff03c3a90b60c44b22734e34dfbd
vasavifullstack <vasavi.fullstack@gmail.com>
2025-12-03 15:48:47 +0530

    example2

---
+++ ConsoleApp1/12.extension_example.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//"Hello world" -- 2
+
+//"Hello world Hello world" -- 4
+
+namespace ExtensitonMethodsNamepsace
+{
+
+    public static class StringExtensions
+    {
+        public static int WordCount(this string input)   //Hello world
+        {
+            string[] words = input.Split(' ');   //["Hello" , "World"]
+            int wordsLength = words.Length;
+            return wordsLength;
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            string sentence = "Hello world";
+            int wordsCount = sentence.WordCount();
+
+            // Displaying the result
+            Console.WriteLine($"Word count: {wordsCount}");
+        }
+    }
+
+}
---
+++ ConsoleApp1/13.extension_example2.cs
@@ -0,0 +1,53 @@
+﻿using Methods_Advanced_Container;
+using MyExtensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+namespace MyExtensions
+
+{
+    // Extension Method-add new method without modifity the existing class.
+    //Existing Class(We cannot change this)
+    //don't change the original class
+    //don't create a derived class
+    //add a method as if it belongs to that class
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    //Rules - Must be inside a static class
+    //Method must be static
+    //First parameter must have the keyword this
+    //First parameter tells which class it extends
+
+
+    //Extension Method Class
+    public static class StudentExtensions
+    {
+        // Extension method to get full name of student
+        public static string GetFullName(this Student student)
+        {
+            return student.FirstName + " " + student.LastName;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Student s = new Student()
+            {
+                FirstName = "Vasavi Reddy",
+                LastName = "Yarramachu"
+            };
+
+            string fullName = s.GetFullName();  // Calling extension method
+
+            Console.WriteLine(fullName);
+        }
+    }
+}
