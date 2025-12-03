a39b95f34c34668ada7cf22e020d0fc103568e52
vasavifullstack <vasavi.fullstack@gmail.com>
2025-12-03 08:55:26 +0530

    extension

--- ConsoleApp1/10.async_param.cs
+++ ConsoleApp1/10.async_param.cs
@@ -1,1 +1,84 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAsyncContainer
+{
+    class UserAsyncClass
+    {
+        //
+        public static async Task Main()
+        {
+            //int id      = await UserAsyncClass.GetStudnetID();
+
+            //string name = await UserAsyncClass.GetStudnetName();
+
+            //bool isMerit = await UserAsyncClass.isMeritStudent();
+
+            Task<int> studentIDTask = UserAsyncClass.GetStudnetID();
+            Task<string> studentNameTask = UserAsyncClass.GetStudnetName();
+            Task<bool> studentIsMeritTask = UserAsyncClass.isMeritStudent();
+
+            await Task.WhenAll(studentIDTask, studentNameTask, studentIsMeritTask);
+
+            int id = studentIDTask.Result;
+            string name = studentNameTask.Result;
+            bool isMerit = studentIsMeritTask.Result;
+
+            Console.WriteLine(id);
+            Console.WriteLine(name);
+            Console.WriteLine(isMerit);
+
+
+        }
+
+
+        //Defination
+        public static async Task<int> GetStudnetID()
+        {
+            //  i have the db record 
+            await Task.Delay(5000);
+            return 101;
+            // i want to resutnr 101
+        }
+
+        //Defaintion
+        public static async Task<string> GetStudnetName()
+        {
+            //  i have the db record 
+            await Task.Delay(10000);
+            return "peter";
+            // i want to resutnr 101
+        }
+
+
+        public static async Task<bool> isMeritStudent()
+        {
+            //  i have the db record 
+            await Task.Delay(8000);
+            return false;
+            // i want to resutnr 101
+        }
+
+
+
+        //Synchrounc call
+        public static async Task<int> GetStudnetAge()
+        {
+            //  i have the db record 
+            //await Task.Delay(1000000);
+            return 20;
+            // i want to resutnr 101
+        }
+
+        //public static int GetStudnetAge()
+        //{
+        //    //  i have the db record 
+        //    //await Task.Delay(1000000);
+        //    return 20;
+        //    // i want to resutnr 101
+        //}
+    }
+}
---
+++ ConsoleApp1/11.extension_method.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+
+
+namespace PhoneSystem
+{
+    /// <summary>
+    /// A basic smartphone
+    /// </summary>
+    public class Smartphone
+    {
+        public string Brand { get; set; }
+        /// <summary>
+        ///  Make a call is a void method 
+        /// </summary>
+        /// <param name="number"></param>
+        public void MakeCallApp(string number)
+        {
+            Console.WriteLine($"Calling {number} from {Brand} smartphone.");
+        }
+    }
+
+    //FitnessApp
+
+    // Note: static class contains only static members
+
+   // extension methods:Method is public static.
+
+  //The first parameter is prefixed with this: this Smartphone myPhone. That tells the compiler this is an extension method for Smartphone.
+  // myPhone is a normal reference to the instance on which the method was called.
+
+  //It cannot access private/protected members.they only use its public (like Brand).
+
+    public static class SmartphoneExtensions
+    {
+        public static void TrackFitnessApp(this Smartphone myPhone)
+        {
+            Console.WriteLine($"The {myPhone.Brand} smartphone is tracking fitness now!");
+        }
+    }
+
+
+    class Program
+    {
+        public static void Main()
+        {
+            Smartphone phone = new Smartphone() { Brand = "Pixel" };
+            phone.MakeCallApp("12345678");
+            phone.TrackFitnessApp();
+        }
+    }
+
+
+
+
+
+}
